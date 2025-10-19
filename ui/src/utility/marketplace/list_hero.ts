bd307e74ec2ad548cdce682b6b192d7db57f07ae
turhanomer <omerturhan16@outlook.com>
2025-10-19 21:37:29 +0300

    Complete all the TODOs

--- ui/src/utility/marketplace/list_hero.ts
+++ ui/src/utility/marketplace/list_hero.ts
@@ -6,17 +6,17 @@ export const listHero = (
   priceInSui: string,
 ) => {
   const tx = new Transaction();
-
-  // TODO: Convert SUI to MIST (1 SUI = 1,000,000,000 MIST)
-  // const priceInMist = ?
-
-  // TODO: Add moveCall to list a hero for sale
-  // Function: `${packageId}::marketplace::list_hero`
-  // Arguments: heroId (object), priceInMist (u64)
-  // Hints:
-  // - Use tx.object() for the hero object
-  // - Use tx.pure.u64() for the price in MIST
-  // - Remember: 1 SUI = 1_000_000_000 MIST
+  
+  // SUI'yi MIST'e çevir (1 SUI = 1,000,000,000 MIST)
+  const priceInMist = BigInt(parseFloat(priceInSui) * 1_000_000_000);
+  
+  tx.moveCall({
+    target: `${packageId}::marketplace::list_hero`,
+    arguments: [
+      tx.object(heroId),
+      tx.pure.u64(priceInMist),
+    ],
+  });
 
   return tx;
 };
